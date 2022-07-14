ddbee706678a600ae690b109440355ac71164f90
jahidcs <csq.jahid@gmail.com>
2022-07-14 21:33:29 +0600

    classes in python oop

---
+++ oop-01.py
@@ -0,0 +1,22 @@
+class Employee:
+    pass
+
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.name = "Jahid Hassan"
+emp_1.email = "jahid@gmail.com"
+emp_1.salary = 6000
+
+emp_2.name = "xay mai"
+emp_2.email = "xyz@gmail.com"
+emp_2.salary = 6000
+emp_2.designation = "Intern"
+
+print(emp_1.email)
+print(emp_2.email)
+print(emp_2.designation)
